428b95c0bf05c3603467fbbb1c4aba3818df5d85
Nickson Kaigi <nickaigi@gmail.com>
2020-01-30 05:22:01 +0300

    stack

---
+++ week_2/LinkedStackOfStrings.java
@@ -0,0 +1,37 @@
+public class LinkedStackOfStrings {
+    private Node first = null;
+
+    private class Node {
+        String item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void push(String item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+    }
+
+    public String pop() {
+        String item = first.item;
+        first = first.next;
+        return item;
+    }
+
+    public static void main(String[] args) {
+        LinkedStackOfStrings stack = new LinkedStackOfStrings();
+        stack.push("to");
+        stack.push("be");
+        stack.push("or");
+        stack.push("not");
+        stack.push("to");
+        stack.push("be");
+
+        System.out.println(stack.pop());
+    }
+}
