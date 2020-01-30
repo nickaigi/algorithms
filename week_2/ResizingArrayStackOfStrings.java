907fd9ec5b927db635afa8e81047ab96c0d36397
Nickson Kaigi <nickaigi@gmail.com>
2020-01-30 20:34:20 +0300

    various array implementations of stack

---
+++ week_2/ResizingArrayStackOfStrings.java
@@ -0,0 +1,22 @@
+public class ResizingArrayStackOfStrings {
+    private String[] s;
+    private int N = 0;
+
+    public ResizingArrayStackOfStrings() {
+        s = new String[1];
+    }
+
+    public void push(String item) {
+        if (N == s.length)
+            resize(2 * s.length);
+        s[N++] = item;
+    }
+
+    private void resize(int capacity) {
+        String[] copy = new String[capacity];
+        for (int i = 0; i < N; i++)
+            copy[i] = s[i];
+
+        s = copy;
+    }
+}
