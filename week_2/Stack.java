d67cc39a873db53b426942f7b1adde4a141cfb7d
Nickson Kaigi <nickaigi@gmail.com>
2020-02-03 06:05:36 +0300

    added an iterator

--- week_2/Stack.java
+++ week_2/Stack.java
@@ -1,9 +1,33 @@
-public class Stack<Item> {
+import java.util.Iterator;
+
+public class Stack<Item> implements Iterable<Item> {
     private Node first = null;
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
 
     private class Node {
         Item item;
         Node next;
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
     }
 
     public boolean isEmpty() {
