daec7789fe2f06d6798fb534875cd1c96701011e
Nickson Kaigi <nickaigi@gmail.com>
2020-03-26 06:54:47 +0300

    new sortcompare

---
+++ week_2/Sorting/SortCompare.java
@@ -0,0 +1,35 @@
+public class SortCompare {
+    // compare two sorting algorithms
+    public static double time(String alg, Double[] a) {
+        Stopwatch timer = new Stopwatch();
+        if (alg.equals("Insertion")) Insertion.sort(a);
+        if (alg.equals("Selection")) Selection.sort(a);
+        if (alg.equals("Shell")) ShellSort.sort(a);
+        return timer.elapsedTime();
+    }
+
+    public static double timeRandomInput(String alg, int N, int T) {
+        //use alg to sort T random arrays of length N
+        double total = 0.0;
+        Double[] a = new Double[N];
+        for (int t = 0; t < T; t++) {
+            // perform one experiment (generate and sort an array).
+            for (int i = 0; i < N; i++) {
+                a[i] = StdRandom.uniform();
+            }
+            total += time(alg, a);
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        String alg1 = args[0];
+        String alg2 = args[1];
+        int N = Integer.parseInt(args[2]);
+        int T = Integer.parseInt(args[3]);
+        double t1 = timeRandomInput(alg1, N, T); // total for alg1
+        double t2 = timeRandomInput(alg2, N, T); // total for alg2
+        StdOut.printf("For %d random Doubles\n     %s is", N, alg1);
+        StdOut.printf(" %.1f times faster than %s\n", t2 / t1, alg2);
+    }
+}
