0fb4ac792bda504a8c348c45f50f81d00d0a4ff8
Nickson Kaigi <nickaigi@gmail.com>
2020-05-12 23:34:23 +0300

    back to basics

--- CollinearPoints/BruteCollinearPoints.java
+++ CollinearPoints/BruteCollinearPoints.java
@@ -9,68 +9,10 @@ public class BruteCollinearPoints {
 
     public BruteCollinearPoints(Point[] points) {
         // finds all line segments containing 4 points
-        if (points == null)
-            throw new IllegalArgumentException();
-        int size = points.length;
-        for (int i = 0; i < size; i++) {
-            if (points[i] == null) {
-                throw new IllegalArgumentException("Null Points not allowed");
-            }
-
-            for (int j = i + 1; j < size; j++) {
-                if (points[j] == null) {
-                    throw new IllegalArgumentException("Null Points not allowed");
-                }
-                if (points[i].compareTo(points[j]) == 0) {
-                    throw new IllegalArgumentException("Same Points");
-                }
-            }
-        }
+        if (points == null) throw new IllegalArgumentException();
     }
 
     private void genCollinearPoints(Point[] points, int size) {
-        for (int p = 0; p < size; p++) {
-            for (int q = 0; q < size; q++) {
-                if (p == q)
-                    continue;
-                if (points[p].compareTo(points[q]) <= 0)
-                    continue;
-                double pSlopeQ = points[p].slopeTo(points[q]);
-                for (int r = 0; r < size; r++) {
-                    if (r == q || r == p)
-                        continue;
-                    if (points[p].compareTo(points[r]) <= 0)
-                        continue;
-                    if (points[q].compareTo(points[r]) <= 0)
-                        continue;
-
-                    double pSlopeR = points[p].slopeTo(points[r]);
-                    if (pSlopeQ != pSlopeR)
-                        continue;
-
-                    for (int s = 0; s < size; s++) {
-                        if (s == r || s == q || s == p)
-                            continue;
-                        if (points[p].compareTo(points[s]) <= 0)
-                            continue;
-                        if (points[q].compareTo(points[s]) <= 0)
-                            continue;
-                        if (points[r].compareTo(points[s]) <= 0)
-                            continue;
-
-                        double pSlopeS = points[p].slopeTo(points[s]);
-                        if (pSlopeS != pSlopeR)
-                            continue;
-
-                        if (segmentsCount == segmentArray.length)
-                            resizeSegArr();
-
-                        segmentArray[segmentsCount++] = new LineSegment(points[p], points[s]);
-
-                    }
-                }
-            }
-        }
     }
 
     public int numberOfSegments() {
@@ -86,32 +28,5 @@ public class BruteCollinearPoints {
          * any input to BruteCollinearPoints that has 5 or more collinear points.
          */
         // the line segments
-        LineSegment[] segmentsCopied = new LineSegment[segmentsCount];
-        int count = 0;
-        for (LineSegment segment : segmentArray) {
-            if (segment != null) {
-                segmentsCopied[count] = segment;
-                count++;
-            }
-        }
-        assert (count == segmentsCount);
-        return segmentsCopied;
-    }
-
-    private void resizeSegArr() {
-        LineSegment[] temp = new LineSegment[segmentArray.length * 2];
-        System.arraycopy(segmentArray, 0, temp, 0, segmentArray.length);
-        segmentArray = temp;
-    }
-
-    public static void main(String[] args) {
-        Point p = new Point(5, 5);
-        Point q = new Point(10, 10);
-        Point r = new Point(15, 15);
-        Point s = new Point(20, 20);
-
-        Point[] points = {p, q, r, s};
-        BruteCollinearPoints bruteCollinearPoints = new BruteCollinearPoints(points);
-
     }
 }
--- CollinearPoints/FastCollinearPoints.java
+++ CollinearPoints/FastCollinearPoints.java
@@ -13,105 +13,13 @@
  * points together. The algorithm is fast because the bottleneck operation is sorting.
  * */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class FastCollinearPoints {
     private int segmentsCount = 0;
-    private int segmentSize = 10;
-    private LineSegment[] segmentArray = new LineSegment[segmentSize];
 
     public FastCollinearPoints(Point[] points) {
         // finds all line segments containing 4 or more points
         if (points == null)
             throw new IllegalArgumentException();
-        int size = points.length;
-
-        for (int i = 0; i < size; i++) {
-            if (points[i] == null)
-                throw new IllegalArgumentException();
-            for (int j = 0; j < size; j++) {
-                if (points[j] == null)
-                    throw new IllegalArgumentException();
-                if (i != j) {
-                    if (points[i].compareTo(points[j]) == 0)
-                        throw new IllegalArgumentException("Same Points");
-                }
-            }
-        }
-    }
-
-    private void resizeSegmentArray() {
-        LineSegment[] copy = new LineSegment[segmentArray.length * 2];
-        System.arraycopy(segmentArray, 0, copy, 0, segmentArray.length);
-        segmentArray = copy;
-    }
-
-    private void genFastCollinearPoints(Point[] points, int size) {
-        // todo()
-        ArrayList<Integer> segmentArrayList = new ArrayList<Integer>();
-        for (int p = 0; p < size; p++) {
-            // swap point[p] with point[0] to always have current item at zeroth index
-            Point tempPoint = points[p];
-            points[p] = points[0];
-            points[0] = tempPoint;
-
-            // copy the array
-            Point[] sorted = points.clone();
-            Arrays.sort(sorted, 1, size, tempPoint.slopeOrder());
-
-            double previousSlope = tempPoint.slopeTo(sorted[1]);
-
-            int segmentLengthTracker = 1;
-
-            segmentArrayList.clear();
-            segmentArrayList.add(0);
-            segmentArrayList.add(1);
-
-            // what start at index 2?
-            int minimumSegment = 2;
-            for (int q = 2; q < size; q++) {
-                double newSlope = tempPoint.slopeTo(sorted[q]);
-                // slope doesn't match the end of the array?
-                if (previousSlope != newSlope || q == size - 1) {
-                    Point lastPoint = sorted[q - 1];
-                    if (q == size - 1 && previousSlope == newSlope) {
-                        lastPoint = sorted[q];
-                        segmentArrayList.add(q);
-                        segmentLengthTracker++;
-                    }
-
-                    // ignore -ve slopes
-                    if (segmentLengthTracker > minimumSegment) {
-                        Point maxP, minP;
-                        maxP = minP = tempPoint;
-                        for (int innerCSegTracker : segmentArrayList) {
-                            if (sorted[innerCSegTracker].compareTo(maxP) > 0)
-                                maxP = sorted[innerCSegTracker];
-
-                            if (sorted[innerCSegTracker].compareTo(minP) <= 0)
-                                minP = sorted[innerCSegTracker];
-                        }
-
-                        if (segmentLengthTracker == segmentArray.length)
-                            resizeSegmentArray();
-                        if (tempPoint.compareTo(minP) == 0) {
-                            segmentArray[segmentsCount] = new LineSegment(minP, maxP);
-                            segmentsCount++;
-                        }
-                    }
-                    segmentLengthTracker = 1;
-                    segmentArrayList.clear();
-                    ;
-                    segmentArrayList.add(0);
-                    segmentArrayList.add(q);
-                } else {
-                    segmentLengthTracker++;
-                    segmentArrayList.add(q);
-                }
-                previousSlope = newSlope;
-            }
-        }
     }
 
 
@@ -122,46 +30,5 @@ public class FastCollinearPoints {
 
     public LineSegment[] segments() {
         // the line segments
-        LineSegment[] copy = new LineSegment[segmentsCount];
-        int count = 0;
-        for (LineSegment segment : segmentArray) {
-            if (segment != null) {
-                copy[count] = segment;
-                count++;
-            }
-        }
-        // were all values copied?
-        assert (count == segmentsCount);
-        return copy;
-    }
-
-    public static void main(String[] args) {
-
-        // read the n points from a file
-        In in = new In(args[0]);
-        int n = in.readInt();
-        Point[] points = new Point[n];
-        for (int i = 0; i < n; i++) {
-            int x = in.readInt();
-            int y = in.readInt();
-            points[i] = new Point(x, y);
-        }
-
-        // draw the points
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(0, 32768);
-        StdDraw.setYscale(0, 32768);
-        for (Point p : points) {
-            p.draw();
-        }
-        StdDraw.show();
-
-        // print and draw the line segments
-        FastCollinearPoints collinear = new FastCollinearPoints(points);
-        for (LineSegment segment : collinear.segments()) {
-            StdOut.println(segment);
-            segment.draw();
-        }
-        StdDraw.show();
     }
 }
